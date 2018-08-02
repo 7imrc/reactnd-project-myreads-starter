f83846f2650a6b082f51358eff6dba4224cdbf75
Tim Campbell <tim.campbell@btinternet.com>
2018-08-02 18:24:33 +0100

    feat: Get list of books and add state
    Add the ability to obtain the list of books via the BooksAPI.  Also
    enabled the state for the list of books.  Tested to be working.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import FrontPage from './FrontPage';
 import SearchBook from './SearchBook';
 class BooksApp extends React.Component {
+
+state = {
+  listOfBooks: []
+}
+
+componentDidMount(){
+  BooksAPI.getAll()
+    .then( (books) => this.setState({
+      listOfBooks: books
+    }))
+}
+/*
   state = {
+*/
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
+/*
     showSearchPage: false
   }
+*/
 
   render() {
+    console.log(this.state.listOfBooks);
+
     return (
       <div className="app">
         <FrontPage />
