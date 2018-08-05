a504184736cd7ae0e864c8734bd39254918d4f6c
Tim Campbell <tim.campbell@btinternet.com>
2018-08-05 18:48:28 +0100

    feat: add React Router link

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import BookInfo from './BookInfo';
+import { Link } from 'react-router-dom';
 
 class FrontPage extends Component {
 
@@ -72,7 +73,9 @@ class FrontPage extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to="/SearchBook">
+            Add a book
+          </Link>
         </div>
       </div>
     );
