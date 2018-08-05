a7fda73ad0ad188ba460539ad458474039705824
Tim Campbell <tim.campbell@btinternet.com>
2018-08-05 18:45:12 +0100

    feat:  Add React router link

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import * as BooksAPI from './BooksAPI';
 import BookInfo from './BookInfo';
 import escapeRegExp from 'escape-string-regexp';
-
+import { Link } from 'react-router-dom';
 
 class SearchBook extends Component {
 
@@ -45,7 +45,9 @@ class SearchBook extends Component {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link to="/" className="close-search">
+            Close
+          </Link>
           <div className="search-books-input-wrapper">
             {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
